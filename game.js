a38da99ab5b4c27ebf4891571ede55d20580e508
Ke Sun <kesun.421@gmail.com>
2016-12-28 11:45:55 -0800

    Hero movement and missile firing working.

--- game.js
+++ game.js
@@ -3,10 +3,11 @@
  */
 
 var stage;
-const ENEMIES_COUNT = 5;
-const SHAPE_WIDTH = 50;
-const SHAPE_HEIGHT = 25;
-const SHAPE_RADIUS = 10;
+var hero;
+const SCREEN_WIDTH = 500, SCREEN_HEIGHT = 500;
+const ENEMIES_COUNT = 8;
+const SHAPE_WIDTH = 50, SHAPE_HEIGHT = 25, SHAPE_RADIUS = 10;
+const KEYCODE_LEFT = 37, KEYCODE_RIGHT = 39, KEYCODE_SPACEBAR = 32;
 
 function init() {
     stage = new createjs.Stage("demoCanvas");
@@ -14,7 +15,9 @@ function init() {
     var enemies = addEnemyAssets();
     applyEnemyAnimations(enemies);
 
-    addHeroAsset();
+    hero = addHeroAsset();
+
+    this.document.onkeydown = keyPressed;
 
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
@@ -41,11 +44,11 @@ function addEnemyAssets() {
 
 function applyEnemyAnimations(enemies) {
     enemies.forEach(function (enemy, index) {
-        enemy.y = 60 * index;
+        enemy.y = 30 * index;
 
         createjs.Tween.get(enemy, {loop: true})
             .to({x:400}, 3000 / (index + 1), createjs.Ease.getPowInOut(2))
-            .to({x:0}, 2000 / (index + 1), createjs.Ease.getPowInOut(2));
+            .to({x:0}, 3000 / (index + 1), createjs.Ease.getPowInOut(2));
     });
 }
 
@@ -56,4 +59,48 @@ function addHeroAsset() {
     hero.y = stage.canvas.height - 30;
 
     stage.addChild(hero);
+
+    return hero;
+}
+
+function heroFire() {
+    console.log("Fire!!!");
+
+    var x = hero.x + SHAPE_WIDTH / 2;
+    var y = hero.y;
+    const MISSILE_RADIUS = 10;
+
+    var missile = new createjs.Shape();
+    missile.graphics.beginFill("rgb(125,125,125)").drawCircle(x, y, MISSILE_RADIUS);
+
+    stage.addChild(missile);
+
+    createjs.Tween.get(missile, {loop: false})
+        .to({y: -SCREEN_HEIGHT}, 1000, createjs.Ease.getPowInOut(2));
+}
+
+function keyPressed(event) {
+    switch(event.keyCode) {
+        case KEYCODE_LEFT:
+            if (hero.x <= 0) {
+                hero.x = 0;
+            }
+            else {
+                hero.x -= 20;
+            }
+            break;
+
+        case KEYCODE_RIGHT:
+            if (hero.x >= SCREEN_WIDTH - SHAPE_WIDTH) {
+                hero.x = SCREEN_WIDTH - SHAPE_WIDTH;
+            }
+            else {
+                hero.x += 20;
+            }
+            break;
+
+        case KEYCODE_SPACEBAR:
+            heroFire();
+            break;
+    }
 }
