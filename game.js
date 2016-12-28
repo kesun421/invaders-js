e047ce685f5f7f5ce31b36c6fd2b2b6517cca3fc
Ke Sun <kesun.421@gmail.com>
2016-12-28 11:21:58 -0800

    Added hero element.  Code refactoring.

--- game.js
+++ game.js
@@ -4,12 +4,17 @@
 
 var stage;
 const ENEMIES_COUNT = 5;
+const SHAPE_WIDTH = 50;
+const SHAPE_HEIGHT = 25;
+const SHAPE_RADIUS = 10;
 
 function init() {
     stage = new createjs.Stage("demoCanvas");
 
     var enemies = addEnemyAssets();
     applyEnemyAnimations(enemies);
+
+    addHeroAsset();
 
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
@@ -20,7 +25,7 @@ function addEnemyAssets() {
 
     for (var i = 0; i < ENEMIES_COUNT; i++) {
         var circle = new createjs.Shape();
-        circle.graphics.beginFill("rgb(255,0,0)").drawRoundRect(0, 0, 50, 25, 10);
+        circle.graphics.beginFill("rgb(255,0,0)").drawRoundRect(0, 0, SHAPE_WIDTH, SHAPE_HEIGHT, SHAPE_RADIUS);
         circle.x = 0;
         circle.y = 0;
 
@@ -42,4 +47,13 @@ function applyEnemyAnimations(enemies) {
             .to({x:400}, 3000 / (index + 1), createjs.Ease.getPowInOut(2))
             .to({x:0}, 2000 / (index + 1), createjs.Ease.getPowInOut(2));
     });
+}
+
+function addHeroAsset() {
+    var hero = new createjs.Shape();
+    hero.graphics.beginFill("rgb(0,0,255)").drawRoundRect(0, 0, SHAPE_WIDTH, SHAPE_HEIGHT, SHAPE_RADIUS);
+    hero.x = stage.canvas.width / 2 - SHAPE_WIDTH / 2;
+    hero.y = stage.canvas.height - 30;
+
+    stage.addChild(hero);
 }
