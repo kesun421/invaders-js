e2a7e87981d2494565ae2ec8cc4202dc12485898
Ke Sun <kesun.421@gmail.com>
2016-12-28 11:08:00 -0800

    Refactor code.

--- game.js
+++ game.js
@@ -8,38 +8,37 @@ const ENEMIES_COUNT = 5;
 function init() {
     stage = new createjs.Stage("demoCanvas");
 
-    var objects = addAssets();
-    console.log("objects: " + objects);
-    applyAnimations(objects);
+    var enemies = addEnemyAssets();
+    applyEnemyAnimations(enemies);
 
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
 }
 
-function addAssets() {
-    var objects = [];
+function addEnemyAssets() {
+    var enemies = [];
 
     for (var i = 0; i < ENEMIES_COUNT; i++) {
         var circle = new createjs.Shape();
-        circle.graphics.beginFill("rgb(255,0,0)").drawRoundRect(0, 0, 50, 35, 10);
+        circle.graphics.beginFill("rgb(255,0,0)").drawRoundRect(0, 0, 50, 25, 10);
         circle.x = 0;
         circle.y = 0;
 
-        objects.push(circle);
+        enemies.push(circle);
     }
 
-    objects.forEach(function (object){
+    enemies.forEach(function (object){
         stage.addChild(object);
     });
 
-    return objects;
+    return enemies;
 }
 
-function applyAnimations(objects) {
-    objects.forEach(function (object, index) {
-        object.y = 60 * index;
+function applyEnemyAnimations(enemies) {
+    enemies.forEach(function (enemy, index) {
+        enemy.y = 60 * index;
 
-        createjs.Tween.get(object, {loop: true})
+        createjs.Tween.get(enemy, {loop: true})
             .to({x:400}, 3000 / (index + 1), createjs.Ease.getPowInOut(2))
             .to({x:0}, 2000 / (index + 1), createjs.Ease.getPowInOut(2));
     });
